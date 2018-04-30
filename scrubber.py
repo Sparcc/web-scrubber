79af32cf37fea85a0acfbd88642c5bc3cc177134
avih8r <sparco.209@gmail.com>
2018-04-30 15:24:53 +1000

    apparently youtube vids are stored on youtube, who knew?

--- scrubber.py
+++ scrubber.py
@@ -1,12 +1,22 @@
 import urllib.request
 import urllib.parse
+import re
 
-url = 'https://www.youtube.com/watch'
-values = {'v':'5GzVNi0oTxQ'}
+url = 'https://www.youtube.com/watch?v=5GzVNi0oTxQ'
+tokenisedUrl = re.search('(https:.*\/watch)(\?([A-Za-z])=([A-Za-z_0-9]*))', url).groups()
+baseUrl = tokenisedUrl[0]
+paramDef = tokenisedUrl[2]
+paramVal = tokenisedUrl[3]
 
-data = urllib.parse.urlencode(values)
-data.encode('utf-8')
-request = urllib.request.Request(url,data)
+print('baseUrl '+baseUrl)
+print('paramDef '+paramDef)
+print('paramVal '+paramVal)
+
+values = {paramDef:paramVal}
+param = urllib.parse.urlencode(values)
+param = param.encode('utf-8')
+headers = {}
+headers['User-Agent'] = 'Mozilla 5.10'
+request = urllib.request.Request(baseUrl,param, headers=headers)
 response = urllib.request.urlopen(request)
-responseData = reponse.read()
-print(responseData)
+print(reponse.read())
